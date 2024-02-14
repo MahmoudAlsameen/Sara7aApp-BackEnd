4374f677be61f78bf59798c6bd40a3a00ed75d3d
Mahmoud Alsameen <mahmoud_abdulrahman1@outlook.com>
2024-02-14 05:53:39 +0200

    update errorMiddleware

--- middleware/errorMiddleware.js
+++ middleware/errorMiddleware.js
@@ -1,10 +1,36 @@
-const globalErrorHandler = (err, req, res, next) => {
-  err.statusCode = err.statusCode || 500;
-  err.status = err.status || "error";
+const sendErrorDev = (err, res) => {
   res.status(err.statusCode).json({
     status: err.status,
     message: err.message,
+    stack: err.stack,
+    error: err,
   });
+};
+
+const sendErrorProd = (err, res) => {
+  if (err.isOperational) {
+    res.status(err.statusCode).json({
+      status: err.status,
+      message: err.message,
+    });
+  } else {
+    console.error("ERROR: ", err);
+    res.sattus(500).json({
+      status: "error",
+      message: "Something went wrong",
+    });
+  }
+};
+
+const globalErrorHandler = (err, req, res, next) => {
+  err.statusCode = err.statusCode || 500;
+  err.status = err.status || "error";
+
+  if (process.env.NODE_ENV === "dev") {
+    sendErrorDev(err, res);
+  } else if (process.env.NODE_ENV === "prod") {
+    sendErrorProd(err, res);
+  }
 };
 
 export default globalErrorHandler;

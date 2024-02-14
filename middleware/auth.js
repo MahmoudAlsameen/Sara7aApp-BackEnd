3ed6bc94bfdc7187faeacf03b2497b1ab99bbc89
Mahmoud Alsameen <mahmoud_abdulrahman1@outlook.com>
2024-02-14 05:06:43 +0200

    fix auth middleware error handling

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,10 +1,9 @@
 import jwt from "jsonwebtoken";
-import AppError from "../utils/appError.js";
 
 const auth = (req, res, next) => {
   let token = req.header("token");
   jwt.verify(token, process.env.JWT_SECRET_KEY, async (err, decoded) => {
-    if (err) next(new AppError(`Invalid token`, 401));
+    if (err) next(err);
     req.userId = decoded.id;
     next();
   });

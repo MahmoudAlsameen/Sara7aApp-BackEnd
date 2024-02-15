463e41333351a73a2c4d0bf8d8e3ff8bea75dc9f
Mahmoud Alsameen <mahmoud_abdulrahman1@outlook.com>
2024-02-15 15:27:45 +0200

    Refactor password hashing and token signing

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -1,11 +1,14 @@
 import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
+import userModel from "../db/models/user.model.js";
 import AppError from "./../utils/appError.js";
 import catchAsync from "./../utils/catchAsync.js";
 
-const saltRounds = process.env.SALT_ROUNDS;
-
 const userRegister = catchAsync(async (req, res, next) => {
-  const hashedPassword = bcrypt.hashSync(req.body.password, saltRounds);
+  const hashedPassword = bcrypt.hashSync(
+    req.body.password,
+    parseInt(process.env.SALT_ROUNDS)
+  );
   const addedUser = await userModel.create({
     ...req.body,
     password: hashedPassword,
@@ -19,7 +22,10 @@ const userLogin = catchAsync(async (req, res, next) => {
   if (queriedUser) {
     const isMatched = bcrypt.compareSync(password, queriedUser.password);
     if (isMatched) {
-      const token = jwt.sign({ id: queriedUser.id }, jwt_secret_key);
+      const token = jwt.sign(
+        { id: queriedUser.id },
+        process.env.JWT_SECRET_KEY
+      );
       res.status(200).json({ message: "Logged in successfully", token });
     } else {
       next(new AppError("Invalid email or password", 401));

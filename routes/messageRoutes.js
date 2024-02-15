f1e16844ae52e1eb9c45495ddaba18d75a5d61a2
Mahmoud Alsameen <mahmoud_abdulrahman1@outlook.com>
2024-02-15 12:08:16 +0200

    add messageRoutes

--- routes/messageRoutes.js
+++ routes/messageRoutes.js
@@ -1,3 +1,16 @@
-const messageRoutes = 1;
+import express from "express";
+import { addMessage, getMessages } from "../controllers/message.controller.js";
+import messageValidationSchema from "../validation/message.validation.js";
+import auth from "./../middleware/auth.js";
+import validation from "./../middleware/validation.js";
+const messageRoutes = express.Router();
+
+messageRoutes.post(
+  "/",
+  validation(messageValidationSchema, "body"),
+  addMessage
+);
+
+messageRoutes.get("/", auth, getMessages);
 
 export default messageRoutes;

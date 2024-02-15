dc05a1ab12cf53c976de9122e7c133c21d52b774
Mahmoud Alsameen <mahmoud_abdulrahman1@outlook.com>
2024-02-15 15:21:03 +0200

    Improve token retrieval and handle missing tokens securely

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,11 +1,17 @@
 import jwt from "jsonwebtoken";
+import AppError from "../utils/appError.js";
 
 const auth = (req, res, next) => {
-  let token = req.header("token");
+  const token = req.headers.token;
+  if (!token) {
+    return next(new AppError("No token provided", 401));
+  }
   jwt.verify(token, process.env.JWT_SECRET_KEY, async (err, decoded) => {
     if (err) next(err);
-    req.userId = decoded.id;
-    next();
+    else {
+      req.userId = decoded.id;
+      next();
+    }
   });
 };
 
